b750c137c8794e7f7d39a091db99a0adda2811bb
Dwane <Dwane@hiphopandcode.com>
2017-04-03 23:42:56 -0400

    Level 2 achieved, barely

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let source = document.querySelector("#ydkjs-quiz-template").innerHTML;
 let template = Handlebars.compile(source);
+let questionsArray =[];
+let buttonsArray=[];
 function Question (inquiry, choices, answer){
   this.inquiry = inquiry;
   this.uniqueId = this.inquiry.slice(0,this.inquiry.length-1).split(" ").join("+");
@@ -7,15 +9,13 @@ function Question (inquiry, choices, answer){
   this.answer = answer;
   this.result = "";
   this.isCorrect = function(event){
-    console.log("this: ",this);
-    let resultId = "#answer-"+this.uniqueId;
-    console.log(resultId);
-    //let this.result = document.querySelector("#answer-"+this.uniqueId);
-    console.log(this.result);
-    console.log("button clicked!");
-    console.log(event.target);
-    console.log(answer);
-    if (event.target.id == answer){
+    let correctAnswer = "";
+    for (i=0;i<questionsArray.length;i++){
+      if(buttonsArray[i].indexOf(event.target.id) != -1){
+        correctAnswer = questionsArray[i].answer;
+      }
+    }
+    if (event.target.id == correctAnswer){
       this.result = "Correct!";
       event.target.style.backgroundColor ="green";
 
@@ -24,9 +24,8 @@ function Question (inquiry, choices, answer){
       event.target.style.backgroundColor ="red";
     }
     let result = document.querySelector("#answer-");
-    result.textContent = this.result;
-    console.log(this.result);
-
+    result.innerHTML = this.result;
+/*
     let quizChoices = document.querySelectorAll(".choice");
     for (i=0; i<quizChoices.length; i++){
       let quizChoice = quizChoices[i];
@@ -36,7 +35,7 @@ function Question (inquiry, choices, answer){
     clone.appendChild(quizChoice.lastChild);
   }
   quizChoice.parentNode.replaceChild(clone, quizChoice);
-  }
+}*/
 /*    let quizChoices = document.querySelectorAll(".choice");
     for (i=0; i<quizChoices.length; i++){
       let quizChoice = quizChoices[i];
@@ -45,15 +44,26 @@ function Question (inquiry, choices, answer){
     }*/
   };
   this.display = function(){
-    console.log("display works");
-    let questionsArray = [
-      question1
+    questionsArray = [
+      question1,
+      question2,
+      question3,
+      question4,
+      question5
     ];
-    console.log(questionsArray);
-    let html = questionsArray.map(object => template(object));//.join("");
+    let html = questionsArray.map(object => template(object)).join("");
     let destination = document.querySelector(".ydkjs-quiz");
     destination.innerHTML = html;
     let quizChoices = document.querySelectorAll(".choice");
+    let buttonSet =[];
+    let buttonsPerQuestion = quizChoices.length / questionsArray.length;
+    for (i=0; i<questionsArray.length; i++){
+      for (j=0; j<buttonsPerQuestion; j++){
+         buttonSet[j] = quizChoices[j+(4*i)].id;
+      }
+      buttonsArray.push(buttonSet);
+      buttonSet=[];
+    }
     for (i=0; i<quizChoices.length; i++){
       let quizChoice = quizChoices[i];
       quizChoice.addEventListener("click",this.isCorrect.bind(this));
@@ -70,4 +80,54 @@ let question1 = new Question (
   ],
 "Oracle"
 );
-question1.display();
+let question2 = new Question (
+  "When did JavaScript first appear?",
+  [
+    "December 4, 1995",
+    "March 12, 1993",
+    "January 23, 1996",
+    "It's always been here."
+  ],
+"December 4, 1995"
+);
+let question3 = new Question (
+  "What was JavaScript officially called when first released?",
+  [
+    "CoffeeScript",
+    "LiveScript",
+    "Java",
+    "Always was JavaScript"
+  ],
+"LiveScript"
+);
+let question4 = new Question (
+  "Who wrote the first JavaScript prototype?",
+  [
+    "Al Gore",
+    "Marc Andreessen",
+    "Steve Jobs",
+    "Brendan Eich"
+  ],
+"Brendan Eich"
+);
+let question5 = new Question (
+  "What internet browser first shipped with JavaScript?",
+  [
+    "Microsoft Internet Explorer",
+    "Netscape Navigator",
+    "Opera",
+    "Safari"
+  ],
+"Netscape Navigator"
+);
+question5.display();
+function nextQuestion(){
+  window.scrollBy(0,800);
+  let result = document.querySelector("#answer-");
+  result.innerHTML = "";
+}
+function previousQuestion(){
+  window.scrollBy(0,-800);
+  let result = document.querySelector("#answer-");
+  result.innerHTML = "";
+}
