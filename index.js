8ce14cbb6d2e824037fc11c5ab650d9b9c824a37
Dwane <Dwane@hiphopandcode.com>
2017-04-05 18:36:16 -0400

    Got the API calls and score keeping working.

--- index.js
+++ index.js
@@ -1,54 +1,84 @@
+let score = 0;
 function Question (inquiry, answer){
   this.inquiry = inquiry;
   this.uniqueId = this.inquiry.slice(0,this.inquiry.length-1).split(" ").join("");
   this.answer = answer;
 }
-function MultipleChoiceQuestion(inquiry, choices, answer){
-  Question.call(this, inquiry, answer);
-  this.choices = choices;
-}
-MultipleChoiceQuestion.prototype.display = function(){
+
+Question.prototype.insertTemplate = function(){
   let source = document.querySelector("#ydkjs-quiz-template").innerHTML;
   let template = Handlebars.compile(source);
   let html = template(this);
   document.querySelector(".ydkjs-quiz").insertAdjacentHTML('beforeend',html);
-  document.querySelector('.ydkjs-quiz .card:last-of-type ul').addEventListener('click', this.isCorrect.bind(this));
-};
-MultipleChoiceQuestion.prototype.isCorrect = function(event){
+
+}
+
+Question.prototype.correctTemplate = function(guess, answer){
   let correctAnswer = "";
-  if (event.target.textContent === this.answer){
+  if (guess === answer){
     this.result = "Correct!";
     event.target.style.backgroundColor ="green";
+    score += 10;
   } else {
     this.result = "No sorry!";
     event.target.style.backgroundColor ="red";
+    score -= 5;
   }
   let result = document.querySelector("#answer-");
   result.innerHTML = this.result;
+  //console.log(score);
+  let runningScore = document.querySelector("#score");
+  runningScore.innerHTML = score;
+  if (score < 0){
+    runningScore.style.color = "red";
+  } else if (score > 0){
+    runningScore.style.color = "green";
+  } else {
+    runningScore.style.color = "black";
+  }
+  event.target.parentElement.classList.add("answered")
+
+}
+
+function MultipleChoiceQuestion(inquiry, choices, answer){
+  Question.call(this, inquiry, answer);
+  this.choices = choices;
+}
+MultipleChoiceQuestion.prototype = Object.create(Question.prototype);
+
+MultipleChoiceQuestion.prototype.display = function(){
+  this.insertTemplate();
+  document.querySelector('.ydkjs-quiz .card:last-of-type ul').addEventListener('click', this.isCorrect.bind(this));
 };
+MultipleChoiceQuestion.prototype.isCorrect = function(event){
+  this.correctTemplate(event.target.textContent, this.answer);
+};
+ShortAnswerQuestion.prototype = Object.create(Question.prototype);
+
 function ShortAnswerQuestion(inquiry, fillIn, answer){
   Question.call(this, inquiry, answer);
   this.fillIn = fillIn;
 }
 ShortAnswerQuestion.prototype.display = function(){
-  let source = document.querySelector("#ydkjs-quiz-template").innerHTML;
-  let template = Handlebars.compile(source);
-  let html = template(this);
-  document.querySelector(".ydkjs-quiz").insertAdjacentHTML('beforeend',html);
+  this.insertTemplate();
   document.querySelector('.ydkjs-quiz .card:last-of-type .submitButton').addEventListener('click', this.isCorrect.bind(this));
 };
 ShortAnswerQuestion.prototype.isCorrect = function(event){
-  let correctAnswer = "";
-  if (event.target.previousElementSibling.value.toLowerCase() === this.answer.toLowerCase()){
-    this.result = "Correct!";
-    event.target.style.backgroundColor ="green";
+  this.correctTemplate(event.target.previousElementSibling.value.toLowerCase(), this.answer.toLowerCase());
+};
 
-  } else {
-    this.result = "No sorry!";
-    event.target.style.backgroundColor ="red";
-  }
-  let result = document.querySelector("#answer-");
-  result.innerHTML = this.result;
+function ApiQuestion(inquiry, choices, answer){
+  Question.call(this, inquiry, answer);
+  this.choices = choices;
+}
+ApiQuestion.prototype = Object.create(Question.prototype);
+
+ApiQuestion.prototype.display = function(){
+  this.insertTemplate();
+  document.querySelector('.ydkjs-quiz .card:last-of-type ul').addEventListener('click', this.isCorrect.bind(this));
+};
+ApiQuestion.prototype.isCorrect = function(event){
+this.correctTemplate(event.target.textContent, this.answer);
 };
 
 //Questions
@@ -119,9 +149,9 @@ let question8 = new ShortAnswerQuestion (
 );
 
 //Randomize questions
-let questionsArray = [question1, question2, question3, question4, question5, question6, question7, question8];
-questionsArray.sort(function(a, b){return 0.5 - Math.random()});
-questionsArray.forEach(question => question.display());
+//let questionsArray = [question1, question2, question3, question4, question5, question6, question7, question8];
+//questionsArray.sort(function(a, b){return 0.5 - Math.random()});
+//questionsArray.forEach(question => question.display());
 
 //Go to the previous or next question
 function nextQuestion(){
@@ -134,3 +164,45 @@ function previousQuestion(){
   let result = document.querySelector("#answer-");
   result.innerHTML = "";
 }
+let totalApiQuestions=[];
+function formatQuestions(questionsObject){
+  questionsObject.incorrect_answers.push(questionsObject.correct_answer);
+  questionsObject.incorrect_answers.sort(function(a, b){return 0.5 - Math.random()});
+  totalApiQuestions.push(new ApiQuestion(questionsObject.question, questionsObject.incorrect_answers, questionsObject.correct_answer));
+  return new ApiQuestion(questionsObject.question, questionsObject.incorrect_answers, questionsObject.correct_answer);
+}
+function displayQuestions(questionsArray){
+  //console.log(totalApiQuestions);
+  //totalApiQuestions.sort(function(a, b){return 0.5 - Math.random()});
+  //totalApiQuestions.forEach(question => question.display());
+  questionsArray.sort(function(a, b){return 0.5 - Math.random()});
+  questionsArray.forEach(question => question.display());
+}
+function combineQuestions(questionsArray){
+  console.log(questionsArray);
+  totalApiQuestions.push(questionsArray);
+}
+function getApiInfo(category,type){
+  console.log(type);
+
+  return fetch("https://opentdb.com/api.php?amount=5&category="+category+"&type="+type)
+  .then(response => response.json())
+  .then(object => object.results.map(formatQuestions))
+  //.then(object => console.log(object));
+  //.then(object => totalApiQuestions.concat(object))
+  .then(displayQuestions)
+  //.then(combineQuestions)
+}
+
+getApiInfo(31,"multiple");
+getApiInfo(31,"boolean")
+//.then(object => console.log(object));
+/*getApiInfo(31,"multiple")
+  .then(getApiInfo(31,"boolean"))
+  //.then(moviePromises => Promise.all(moviePromises))
+
+  .then(object => console.log(object));
+  //.then(displayQuestions)
+  console.log(totalApiQuestions);
+//.then(object => console.log(object));
+*/
